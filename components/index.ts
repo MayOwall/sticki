6c0e0625b79784e9f935877ed8ec76c663680f81
Owall <97934878+MayOwall@users.noreply.github.com>
2022-10-12 15:46:15 +0900

    fix : index.ts 파일 및 component index.ts 수정

--- components/index.ts
+++ components/index.ts
@@ -2,9 +2,22 @@ import { BaseContainer } from "./atoms/BaseContainer"
 import { Icon } from "./atoms/Icon"
 import { Logo } from "./atoms/Logo"
 import { SignButton } from "./atoms/SignButton"
+import { CurrentNumber } from "./atoms/CurrentNumber"
+import { DashBoardButton } from "./atoms/DashBoardButton"
+import { Greeting } from "./atoms/Greeting"
 
 import { SignInput } from "./molecules/SignInput"
 
 import { SignForm } from "./organisms/SignForm"
 
-export { BaseContainer, Icon, Logo, SignButton, SignInput, SignForm }
+export {
+  BaseContainer,
+  Icon,
+  Logo,
+  SignButton,
+  SignInput,
+  SignForm,
+  CurrentNumber,
+  Greeting,
+  DashBoardButton,
+}
